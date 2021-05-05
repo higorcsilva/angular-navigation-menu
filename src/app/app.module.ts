135fdaf4a3e0a2007f0f0524c6d294c63056649a
higorcsilva <higorsilva.ads@gmail.com>
2021-05-05 17:48:38 -0300

    teste

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationMenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
